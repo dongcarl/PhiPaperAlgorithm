9329e44c5a378c1bd9dc794f796ab9665e373292
Carl Dong <dongcarl@yahoo.co.uk>
2014-04-22 17:52:18 -0400

    Added SetFamily Class

---
+++ src/edu/choate/SetFamily.java
@@ -0,0 +1,15 @@
+package edu.choate;
+
+import java.util.AbstractSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by dongcarl on 4/22/14.
+ */
+public class SetFamily extends LinkedHashSet<LinkedHashSet<Integer>>
+{
+
+
+
+}
