ba209b54e6fdb2065836bb7f85844d829da03e69
Matthew Bardoe <mbardoe@gmail.com>
2014-04-25 10:08:59 -0400

    Add DeltaFreeSystem class
    Added a class to represent a family of sets that is delta system free.

---
+++ src/edu/choate/DeltaFreeSystem.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.choate;
+
+/**
+ * This class simulates a family of sets that is delta(n) free for some 
+ * given value of n.
+ * @author mbardoe
+ *
+ */
+public class DeltaFreeSystem extends SetFamily {
+
+	/**
+	 * @param n is the size of each set in the family
+	 * @param k is the size the deltasystem that is not allowed.
+	 * @param incomingE
+	 */
+	public DeltaFreeSystem(int n, int k, SetFamily incomingE) {
+		super(n, incomingE);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	public DeltaFreeSystem() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
