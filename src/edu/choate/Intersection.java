7b1bcc9a8ff95fad29f323d7aa2317cfe1014389
Carl Dong <dongcarl@yahoo.co.uk>
2014-05-05 11:12:49 -0400

    Modified Intersection.java

--- src/edu/choate/Intersection.java
+++ src/edu/choate/Intersection.java
@@ -1,23 +1,39 @@
 package edu.choate;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 /**
  * Created by dongcarl on 4/29/14.
  */
 public class Intersection extends IntegerSet
 {
-	static ArrayList<Integer> intersection;
-    public IntegerSet alphaSet;
-    public IntegerSet betaSet;
+    public IntegerSet alphaVertex;
+    public IntegerSet betaVertex;
 
-	public Intersection(IntegerSet intSet1, IntegerSet intSet2)
-	{
-		super();
-		intersection = intSet1.intersection(intSet2);
 
-        alphaSet = intSet1;
-        betaSet = intSet2;
-	}
+    public Intersection(IntegerSet incomingAlphaVertex, IntegerSet incomingBetaVertex)
+    {
+        super();
+        alphaVertex = incomingAlphaVertex;
+        betaVertex = incomingBetaVertex;
+        this.addAll(alphaVertex.intersection(betaVertex));
+    }
+
+    public IntegerSet toIntegerSet()
+    {
+        ArrayList<Integer> listFromIntersection = new ArrayList<Integer>();
+        listFromIntersection.addAll(this);
+        return new IntegerSet(listFromIntersection);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj.getClass() == Intersection.class)
+        {
+            Intersection edge = (Intersection)obj;
+            return ((this.alphaVertex == edge.alphaVertex && this.betaVertex == edge.betaVertex) || (this.alphaVertex == edge.betaVertex && this.betaVertex == edge.alphaVertex));
+        }
+        return false;
+    }
 }
